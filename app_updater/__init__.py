166cc41786abd09e8e8581a8c75d9a0d61873c02
CrinitusFeles <crinitusfeles@gmail.com>
2024-11-15 12:09:55 +0700

    added changelog

--- app_updater/__init__.py
+++ app_updater/__init__.py
@@ -1,3 +1,5 @@
 from .update_checker_widget import UpdateCheckerDialog  # noqa: F401
 from .check_for_update import (check_for_updates, get_releases_list,  # noqa: F401
                                get_latest_release)
+
+__version__ = '0.1.0'
